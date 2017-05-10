1d12f53692d5f9cc76aa6fbd0eee211243369463
Megan Coyle <mcoyle@usgbc.org>
2017-05-10 09:27:55 -0400

    Move records to database

--- js/controllers/meetings.js
+++ js/controllers/meetings.js
@@ -1,3 +1,27 @@
-myApp.controller('MeetingsController', ['$scope', function($scope) {
-  $scope.message = "Success!";
-}]);
+myApp.controller('MeetingsController',
+  ['$scope', '$firebaseAuth', '$firebaseArray',
+  function($scope, $firebaseAuth, $firebaseArray) {
+
+    // reference to database
+    var ref = firebase.database().ref();
+    // reference authentication (gives access to different properties)
+    var auth = $firebaseAuth();
+
+    // handles when the state of authentication changes for a user
+    auth.$onAuthStateChanged(function(authUser) {
+      if(authUser) {
+        var meetingsRef = ref.child('users').child(authUser.uid).child('meetings');
+        var meetingsInfo = $firebaseArray(meetingsRef);
+
+        $scope.addMeeting = function() {
+          meetingsInfo.$add({
+            name: $scope.meetingname,
+            date: firebase.database.ServerValue.TIMESTAMP
+          }).then(function() {
+            $scope.meetingname='';
+          }); //promise
+        } //addMeeting
+      } //authUser
+    }); //onAuthStateChanged
+
+}]); //myApp.controller
