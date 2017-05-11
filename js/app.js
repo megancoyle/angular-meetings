68a2855981ab5f40183f5c5bc3580164c18d165c
Megan Coyle <mcoyle@usgbc.org>
2017-05-11 09:26:47 -0400

    Add checkins

--- js/app.js
+++ js/app.js
@@ -19,6 +19,14 @@ myApp.config(['$routeProvider', function($routeProvider) {
       templateUrl: 'views/register.html',
       controller: 'RegistrationController'
     }).
+    when('/checkins/:uId/:mId', {
+      templateUrl: 'views/checkins.html',
+      controller: 'CheckInsController'
+    }).
+    when('/checkins/:uId/:mId/checkinsList', {
+      templateUrl: 'views/checkinslist.html',
+      controller: 'CheckInsController'
+    }).
     when('/meetings', {
       templateUrl: 'views/meetings.html',
       controller: 'MeetingsController',
---
+++ js/controllers/checkins.js
@@ -0,0 +1,30 @@
+myApp.controller('CheckInsController',
+  ['$scope', '$rootScope', '$location', '$routeParams', '$firebaseObject', '$firebaseArray',
+  function($scope, $rootScope, $location, $routeParams, $firebaseObject, $firebaseArray) {
+
+    var ref, checkinsList;
+
+    $scope.whichmeeting = $routeParams.mId;
+    $scope.whichuser = $routeParams.uId;
+
+    ref = firebase.database().ref()
+      .child('users').child($scope.whichuser)
+      .child('meetings').child($scope.whichmeeting)
+      .child('checkins');
+
+    checkinsList = $firebaseArray(ref);
+    $scope.checkins = checkinsList;
+
+    $scope.addCheckin = function() {
+      $firebaseArray(ref).$add({
+        firstname: $scope.user.firstname,
+        lastname: $scope.user.lastname,
+        email: $scope.user.email,
+        date: firebase.database.ServerValue.TIMESTAMP
+      }).then(function() {
+        $location.path('/checkins/' + $scope.whichuser + '/' +
+        $scope.whichmeeting + '/checkinsList')
+      }) //$add
+    } //addCheckin
+
+}]); //myApp.controller
